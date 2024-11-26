d508069b4e354298dd1e934045a5eccf575ccb13
JohnDelcano <bhoxse.delcano@gmail.com>
2024-11-26 22:16:46 +0800

    realtime database

--- FIREBASECONFIG.js
+++ FIREBASECONFIG.js
@@ -1,22 +1,27 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore} from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCSRrlZZK-2gr97FkYx9kVkMfwPdyuVOOA",
     authDomain: "fir-integration-react.firebaseapp.com",
+    databaseURL: "https://fir-integration-react-default-rtdb.firebaseio.com/",
     projectId: "fir-integration-react",
     storageBucket: "fir-integration-react.appspot.com",
     messagingSenderId: "902088203044",
-    appId: "1:902088203044:web:a268055ea9e2b05bbf31b1",
+    appId: "1:902088203044:web:a268055ea9e2b05bbf31b1",     
     measurementId: "G-3BVJJ3GEWT"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const rtdb = getDatabase(app);
 const auth = initializeAuth(app, { 
     persistence: getReactNativePersistence(ReactNativeAsyncStorage) 
 });
 
-export { app, auth };
+export { app, auth, db, rtdb };
